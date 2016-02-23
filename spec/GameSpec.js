b5f4455e662563ec635754bf97e8a117b461d386
Heather <campbell.hf@gmail.com>
2016-02-23 12:25:07 +0000

    p1 and computer can select a weapon choice

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,5 +6,27 @@ describe("Game", function(){
     game = new Game();
   });
 
+  describe('Player 1', function(){
+
+    it("allows p1 to select a weapon of choice", function(){
+      game.p1SelectWeapon('rock');
+      expect(game.p1Weapon).toEqual('rock');
+    });
+
+    it("throws an error if p1 selects a weapon other than R, P or S", function(){
+      expect(function(){game.p1SelectWeapon('lizard');}).toThrow ('not a valid weapon choice, please choose from either rock, paper or scissors')
+    });
+
+  });
+
+  describe('Computer', function(){
+
+    it("selects a random weapon from the weaponChoices array", function(){
+      spyOn(game, 'computerSelectWeapon').and.returnValue(game.computerWeapon = 'scissors');
+      expect(game.computerWeapon).toEqual('scissors');
+    });
+
+
+  });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,18 @@
 function Game() {
-
+  this.p1Weapon = null;
+  this.computerWeapon = null;
+  this.weaponChoices = ['rock', 'paper', 'scissors'];
 }
+
+  Game.prototype.p1SelectWeapon = function(weapon){
+    if (this.weaponChoices.indexOf(weapon) > -1){
+      this.p1Weapon = weapon;
+    }
+    else {
+      throw 'not a valid weapon choice, please choose from either rock, paper or scissors'
+    }
+  };
+
+  Game.prototype.computerSelectWeapon = function(){
+    this.computerWeapon = this.weaponChoices[Math.floor(Math.random()*this.weaponChoices.length)];
+  };
