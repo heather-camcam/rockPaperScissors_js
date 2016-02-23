f8d23369bf1cb88f6cb56f6209e75daad14aefd4
Heather <campbell.hf@gmail.com>
2016-02-23 18:38:05 +0000

    interface not working

--- js/interface.js
+++ js/interface.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
 
   var game = new Game();
 
+  $('#setP1Name').submit(function(event) {
+    event.preventDefault();
+    var name = $('#p1Name').val();
+    game.setP1Name(name);
+    $('#Name').text(game.p1Name);
+  });
+
 });
